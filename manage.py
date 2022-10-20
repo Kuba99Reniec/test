7d1e407e872eda2102a773438c19d85ed650b8a7
Kuba99Reniec <110913509+Kuba99Reniec@users.noreply.github.com>
2022-10-20 13:35:30 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,3 +1,7 @@
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.graph_objects as go
 import pandas as pd
 
