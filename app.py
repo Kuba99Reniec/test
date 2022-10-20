1395416953bf3190ffb98c7236724e474f03aa9a
Kuba99Reniec <110913509+Kuba99Reniec@users.noreply.github.com>
2022-10-20 13:03:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,82 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objs as go
+import plotly.graph_objects as go
+import pandas as pd
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+dane_cawi_capi = pd.read_csv("https://raw.githubusercontent.com/Kuba99Reniec/wykresy/master/dane_do_dasha.txt")
+options = []
+for col in dane_cawi_capi.columns[:-1]:
+    options.append({'label':'{}'.format(col, col), 'value':col})
+value = 'Wielkość gospodarstwa domowego (6 kategorii)'
+zmienne = pd.read_csv("https://raw.githubusercontent.com/Kuba99Reniec/wykresy/master/5_opis_zmiennych_20220819_podwojny_konwerter_opinie.txt", sep="\t")
+wartosci = pd.read_csv("https://raw.githubusercontent.com/Kuba99Reniec/wykresy/master/6.txt", sep="\t", decimal =",")
+wartosci = wartosci.merge(zmienne, on = "Nazwa zmiennej")
+zmienna = 'RS: Płeć'
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
-
-########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
-
-########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
+app.layout = html.Div([
+    html.H3('Porównanie rozkładów zmiennych: CAWI vs CAWI + CAPI (IX 2021 - VIII 2022)'),
+    html.Label("Wybierz zmienną:"),
+    dcc.Dropdown(
+            id = 'my_dropdown',
+            options= options,
+            value='Choose columns'
+        ),
+    html.Label(id='my_label'),
     dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
-    ]
+        id='graph',
+        figure=go.Figure()
+    )
+])
+@app.callback(
+    Output('graph', 'figure'),
+    Input('my_dropdown', 'value'),
+    State('graph', 'figure'),
 )
+def update_figure(value, fig):
+    zmienna = value
+    df_pom = pd.DataFrame(dane_cawi_capi[dane_cawi_capi['badanie'] == 'cawi'][zmienna].value_counts())
+    names_cawi_pom = df_pom.index.to_list()
+    values_cawi = df_pom[zmienna].to_list()
+    values_cawi_capi = []
+    names_cawi = []
+    for i in names_cawi_pom:
+        values_cawi_capi.append(len(dane_cawi_capi[dane_cawi_capi[zmienna] == i]))
+        if zmienna in wartosci['Etykieta zmiennej'].to_list():
+            names_cawi.append(
+                wartosci[(wartosci['Etykieta zmiennej'] == zmienna) & (wartosci['wartość'] == i)]['Etykieta'].to_list()[
+                    0])
+        else:
+            names_cawi = names_cawi_pom
+    fig = go.Figure()
+    fig.add_trace(go.Funnelarea(
+        name="CAWI",
+        scalegroup="first",
+        text=names_cawi,
+        values=values_cawi,
+        showlegend=False,
+        marker={"colors": ["lawngreen", "darkorange", "red", "yellow", "blue", "peru", "darkviolet"]},
+        title={"position": "top center", "text": "CAWI"},
+    ))
+    fig.add_trace(go.Funnelarea(
+        name="CAWI + CAPI",
+        scalegroup="third",
+        text=names_cawi,
+        values=values_cawi_capi,
+        textinfo="percent",
+        title={"position": "top center", "text": "CAWI+CAPI"},
+        showlegend=False,
+        domain={"x": [.75, 1]}
+    ))
+    fig.update_layout(
+        autosize=False,
+        width=1150,
+        height=600)
+    fig.update_traces(title_font_size=20)
+    fig.update_traces(title_position='top center')
+    fig.update_layout(
+        margin=dict(l=20, r=20, t=20, b=25),
+    )
+    return fig
 
 if __name__ == '__main__':
-    app.run_server()
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
+    app.run_server(debug=True)
